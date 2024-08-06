3723edf5ac75fd41fe269c64a9884e20c0ead62a
Xintonggg <141611314+Xintonggg@users.noreply.github.com>
2024-08-06 00:03:23 -0600

    update log in page

--- app/logIn/page.js
+++ app/logIn/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import Link from 'next/link';
 import { useEffect, useState } from "react";
 import { useUserAuth } from "./_utils/auth-context";
 
@@ -23,28 +23,53 @@ export default function SignInPage() {
   }
 
   return (
-    <main>
-      <header>
-        <h1 className="text-3xl">Firebase Auth</h1>
-      </header>
+    <main className='h-screen flex justify-center items-center'
+    style={{
+      backgroundImage: 'url("/main.jpg")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+     
+    }}
+  >
+
       {user ? (
         // user IS logged in
-        <div>
-          <p>Welcome {user.displayName}</p>
-          <p>{user.email}</p>
-          <img className="w-8 h-8" src={user.photoURL} />
+
+
+        <div className='h-96 w-96 shadow-lg p-8 bg-green-50 flex flex-col items-center rounded-lg'>
+          <p className='text-2xl font-bold'>Welcome {user.displayName}</p>
+          
+          <img className="w-14" src={user.photoURL} />
+          
+          <button className="bg-green-300 text-white px-4 py-2 rounded"> 
+                        <Link href={"./homepage/"}>Go To HomePage </Link>
+          </button>
+
           <button onClick={handleSignOut} className="text-lg m-2 hover:underline">Sign Out</button>
         </div>
       ) : (
+
+
+
+        
         // user IS NOT logged in
-        <div>
+        <div className='h-96 w-96 shadow-lg p-8 bg-green-50 flex flex-col items-center rounded-lg'>
+          <div className="flex items-center">
+            <img src="/logo.jpg" alt="Description of the image" className="mr-2 h-10" />
+            <h1 className='font-bold'>BOOKIE</h1>
+           
+          </div>
+          
+
+          <h1>Log in by Github</h1>
           <button
             onClick={handleSignIn}
-            className="text-lg m-2 hover:underline"
+            className="text-lg mt-4 hover:underline bg-white rounded-md px-5"
           >
             Sign In
           </button>
         </div>
+
       )}
     </main>
   );
